1cd97045d175ea4135eff22accd6cb1eebc4e81c
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2021-10-27 09:29:11 -0300

    Removing depreciate colors

--- LetrandoUIKit/Sources/LetrandoUIKit/Extensions/Colors.swift
+++ LetrandoUIKit/Sources/LetrandoUIKit/Extensions/Colors.swift
@@ -10,22 +10,6 @@ import UIKit
 public extension UIColor {
     
     static let transparentLightBlue = UIColor(red: 90/255, green: 200/255, blue: 250/255, alpha: 0.50)
-    
-    @available(*, deprecated, message: "use lightGreenBackgroundLetrando")
-    static let lightGreenBack = UIColor(red: 208/255, green: 227/255, blue: 219/255, alpha: 1)
-    
-    @available(*, deprecated, message: "use customBrown")
-    static let bronwLetters = UIColor(red: 111/255, green: 96/255, blue: 94/255, alpha: 1)
-    
-    @available(*, deprecated, message: "use greenActionLetrando")
-    static let greenButtons = UIColor(red: 96/255, green: 193/255, blue: 96/255, alpha: 1)
-    
-    static let purpleLetters = UIColor(red: 143/255, green: 97/255, blue: 168/255, alpha: 1)
-    
-    @available(*, deprecated)
-    static let whiteViews = UIColor(red: 242/255, green: 242/255, blue: 242/255, alpha: 1)
-    
-    // MARK: - New Colors
     static let lightGreenBackgroundLetrando = UIColor(red: 239/255, green: 249/255, blue: 245/255, alpha: 1)
     static let customBrown = UIColor(red: 111/255, green: 96/255, blue: 94/255, alpha: 1)
     static let greenActionLetrando = UIColor(red: 96/255, green: 193/255, blue: 96/255, alpha: 1)
