9184762a09ded982e0d6308853b260c4f22d72e8
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2021-10-28 11:23:52 -0300

    Create new project and add package manager

---
+++ ProjectExample/ProjectExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectExample
+//
+//  Created by Ronaldo Gomes on 28/10/21.
+//
+
+import UIKit
+import LetrandoUIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .green
+    }
+}
+
